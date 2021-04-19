f4311d26ee8da8d4886ef6799e5abb46e71c3da7
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2021-04-18 18:37:34 -0700

    init

---
+++ Assets/Scripts/Physics.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour
+{
+    private const float pi = Mathf.PI;
+
+    private const float coeff = 0.083333f;
+
+    public float angularMomentum; //vector
+
+    public float mass;
+
+    private float length;
+
+
+    public float momentOfInertia; //vector
+
+    public float angularVelocity;
+
+    void Awake()
+    {
+        /*angularMomentum = 1.0f;
+        mass = 1.0f;
+        length = 1.0f;
+        angularVelocity = 1.0f;
+        angularAcceleration = 1.0f;
+        momentOfInertia = 1.0f;*/
+
+        if (name == "Weight1" || name == "Weight3")
+        {
+            length = 2.0f;
+        }
+        else if (name == "Weight2" || name == "Weight4")
+        {
+            length = 4.0f;
+        }
+    }
+
+    void getMomentOfInertia()
+    {
+        momentOfInertia = coeff * mass * Mathf.Pow(length, 2);
+    }
+
+    void getAngularMomentum()
+    {
+        angularMomentum = momentOfInertia * angularVelocity;
+    }
+
+    void getAngularVelocity()
+    {
+        angularVelocity = angularMomentum / momentOfInertia;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        getMomentOfInertia();
+        //getAngularMomentum();
+        getAngularVelocity();
+        float angVelConverted = angularVelocity * (180/pi); 
+        transform.Rotate(Vector3.down * Time.deltaTime * angVelConverted);
+    }
+
+
+
+}
